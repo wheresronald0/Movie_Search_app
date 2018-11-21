3d2f09e2b411989ee274658e999bc180293acac5
Ron Mason <WHERESRONALD0@GMAIL.COM>
2018-11-21 16:16:07 -0500

    finish product with styling

--- app.js
+++ app.js
@@ -5,21 +5,39 @@ const Movie = props => {
       style={{
         display: "flex",
         flexDirection: "row",
-        height: "400px",
-        width: "650px",
-        margin: "0 10px 0 10px"
+        height: "350px",
+        width: "600px",
+        margin: "0 10px 0 35px"
       }}
     >
       <div>
         <img
           src={props.img}
-          style={{ height: "250px", width: "auto", marginRight: "20px" }}
+          style={{
+            height: "250px",
+            width: "auto",
+            margin: "23px 20px 0px 0px"
+          }}
         />
       </div>
       <div>
-        <h3>{props.name}</h3>
-        <p>Genre: {props.genre}</p>
-        <p>{props.description}</p>
+        <h3 style={{ marginBottom: "0" }}>{props.name}</h3>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            margin: "0"
+          }}
+        >
+          <p style={{ marginRight: "20px" }}>
+            Genre: <strong>{props.genre}</strong>
+          </p>
+          <p>
+            Rating: <strong>{props.rating}</strong>
+          </p>
+        </div>
+
+        <p style={{ marginTop: "0" }}>{props.description}</p>
       </div>
     </div>
   );
@@ -28,18 +46,33 @@ const Movie = props => {
 // movies component containing all movies
 const Movies = props => {
   return (
-    <div style={{ display: "flex", flexDirection: "row", flexWrap: "wrap" }}>
-      {props.allMovies.map(movie => {
-        return (
-          <Movie
-            name={movie.name}
-            genre={movie.genre}
-            description={movie.description}
-            img={movie.img}
-            key={movie.id}
-          />
-        );
-      })}
+    <div>
+      <ReactTransitionGroup.TransitionGroup
+        style={{ display: "flex", flexDirection: "row", flexWrap: "wrap" }}
+      >
+        {props.allMovies.map(movie => {
+          return (
+            <ReactTransitionGroup.CSSTransition
+              key={movie.id}
+              classNames={{
+                enter: "animated",
+                enterActive: "fadeIn",
+                exit: "animated",
+                exitActive: "fadeOut"
+              }}
+              timeout={1000}
+            >
+              <Movie
+                name={movie.name}
+                genre={movie.genre}
+                rating={movie.rating}
+                description={movie.description}
+                img={movie.img}
+              />
+            </ReactTransitionGroup.CSSTransition>
+          );
+        })}
+      </ReactTransitionGroup.TransitionGroup>
     </div>
   );
 };
@@ -47,26 +80,63 @@ const Movies = props => {
 //serach bar functional component
 const SearchBar = props => {
   const genres = window.MoviesDirectory.genres;
+  const ratings = window.MoviesDirectory.ratings;
 
-  const newSearchData = evt => {
-    props.movieSearchHandler(evt.target.value);
+  //need three handlers for name, genre, and coming soon (maybe a rating?)
+  const newSearchName = evt => {
+    props.movieSearchHandler("name", evt.target.value);
   };
+  const newSearchGenre = evt => {
+    props.movieSearchHandler("genre", evt.target.value);
+  };
+  const newSearchRating = evt => {
+    props.movieSearchHandler("rating", evt.target.value);
+  };
+  const newSearchComingSoon = evt => {
+    props.movieSearchHandler("comingSoon", evt.target.checked);
+  };
+
   return (
     <div
       style={{
-        margin: "20px 0 30px 20px"
+        margin: "-1px 0 30px 0",
+        padding: "15px 20px 0 20px",
+        boxShadow: "0px .1px 5px 0px grey",
+        height: "90px"
       }}
     >
       <form>
-        <h3>Search for Movies Currently Playing </h3>
-        <input type="text" onChange={newSearchData} />
-        <select>
-          <option>Select</option>
+        <h3 style={{ marginTop: "0px" }}>
+          Search for Movies Currently Playing{" "}
+        </h3>
+
+        <label>Movie Name </label>
+        <input
+          type="text"
+          onChange={newSearchName}
+          style={{ margin: " 0px 30px 20px 0px" }}
+        />
+
+        <label>Genre </label>
+        <select style={{ marginRight: "30px" }} onChange={newSearchGenre}>
+          <option>Select </option>
           {genres.map(genre => {
             return <option key={genre}>{genre}</option>;
           })}
         </select>
-        <input type="checkbox" />
+
+        <label>Rating </label>
+        <select style={{ marginRight: "30px" }} onChange={newSearchRating}>
+          <option>Select </option>
+          {ratings.map(rating => {
+            return <option key={rating}>{rating}</option>;
+          })}
+        </select>
+
+        <label>Coming Soon </label>
+        <input type="checkbox" onChange={newSearchComingSoon} />
+
+        {/* <input>Clear Search</input> */}
       </form>
     </div>
   );
@@ -79,27 +149,42 @@ class MovieDirectory extends React.Component {
       allMovies: window.MoviesDirectory.movie,
       name: "",
       genre: "",
+      rating: "",
       id: "",
       description: "",
       comingSoon: false
     };
+    this.movieSearchHandler = this.movieSearchHandler.bind(this);
   }
 
-  movieSearchHandler = movieSearchFor => {
-    this.setState({ allMovies: movieSearchFor });
-  };
-
-  movieSearchHandler = movieSearchFor => {
-    this.setState({ allMovies: movieSearchFor });
+  movieSearchHandler = (theKey, val) => {
+    this.setState({ [theKey]: val }, this.updateMovies);
   };
 
   //get data back from child search
-  //filter through APT and return new array
+  //filter through API and return new array
+  updateMovies() {
+    const moviesFiltered = window.MoviesDirectory.movie.filter(
+      function(movie) {
+        return (
+          movie.comingSoon === this.state.comingSoon &&
+          //initial movie load is the full list from the API, and isn't effected by this .filter until you engage it
+          (this.state.name === "" ||
+            //this.state.name is empty starting point that's always true saying 'you can toggle, and once the onChange takes over, that's when the partial matching begins
+            movie.name.toLowerCase().indexOf(this.state.name.toLowerCase()) !==
+              -1) &&
+          (this.state.genre === "" || movie.genre === this.state.genre) &&
+          (this.state.rating === "" || movie.rating === this.state.rating)
+        );
+      }.bind(this)
+    );
+
+    this.setState({
+      allMovies: moviesFiltered
+    });
+  }
 
   render() {
-    //     const listFilter = allMovies.filter((movieList) => {
-    // if ()
-    //     });
     return (
       <div>
         <SearchBar
